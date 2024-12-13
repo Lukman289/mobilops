e805225e0884446bfb31aaac77da467a9073737d
Lukman_28 <lukmaneka216@gmail.com>
2024-12-13 21:45:23 +0700

    upload new project

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mobil Operations</title>
+</head>
+<body>
+    <h1>Welcome to Mobil Operations</h1>
+    <p>Click <a href="">here</a> to login</p>
+    <p>Click <a href="">here</a> to register</p>
+</body>
+</html>
